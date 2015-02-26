b599b31749dce67d3d0ccffb76201b44fc8b3fae
Felipe Dornelas <felipead@gmail.com>
2015-02-25 22:21:23 -0300

    Fixed CellName generation.

--- src/main/java/org/apache/cassandra/logger/LogMutationBuilder.java
+++ src/main/java/org/apache/cassandra/logger/LogMutationBuilder.java
@@ -1,9 +1,11 @@
 package org.apache.cassandra.logger;
 
+import org.apache.cassandra.cql3.ColumnIdentifier;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.db.composites.CellName;
 import org.apache.cassandra.db.composites.CellNames;
-import org.apache.cassandra.utils.ByteBufferUtil;
+
+import static org.apache.cassandra.utils.ByteBufferUtil.bytes;
 
 public class LogMutationBuilder {
     
@@ -17,16 +19,16 @@ public class LogMutationBuilder {
     
     public Mutation build(LogEntry logEntry) {
         long timestamp = System.currentTimeMillis();
-        Mutation mutation = new Mutation(keyspace, ByteBufferUtil.bytes(logEntry.getKey()));
-        mutation.add(columnFamily, toCellName("target_keyspace"), ByteBufferUtil.bytes(logEntry.getTargetKeyspace()), timestamp);
-        mutation.add(columnFamily, toCellName("target_column_family"), ByteBufferUtil.bytes(logEntry.getTargetColumnFamily()), timestamp);
+        Mutation mutation = new Mutation(keyspace, bytes(logEntry.getKey()));
+        mutation.add(columnFamily, toCellName("target_keyspace"), bytes(logEntry.getTargetKeyspace()), timestamp);
+        mutation.add(columnFamily, toCellName("target_column_family"), bytes(logEntry.getTargetColumnFamily()), timestamp);
         mutation.add(columnFamily, toCellName("target_partition_key"), logEntry.getTargetPartitionKey(), timestamp);
-        mutation.add(columnFamily, toCellName("operation_type"), ByteBufferUtil.bytes(logEntry.getOperationType().name()), timestamp);
-        mutation.add(columnFamily, toCellName("operation_timestamp"), ByteBufferUtil.bytes(logEntry.getOperationTimestamp()), timestamp);
+        mutation.add(columnFamily, toCellName("operation_type"), bytes(logEntry.getOperationType().name()), timestamp);
+        mutation.add(columnFamily, toCellName("operation_timestamp"), bytes(logEntry.getOperationTimestamp()), timestamp);
         return mutation;
     }
 
     private CellName toCellName(String name) {
-        return CellNames.simpleDense(ByteBufferUtil.bytes(name));
+        return CellNames.simpleSparse(new ColumnIdentifier(name, false));
     }
 }
