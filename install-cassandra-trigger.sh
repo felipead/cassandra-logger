eb55942cdde83d416110e78e0991dd8fde74f68f
Felipe Dornelas <felipead@gmail.com>
2015-02-26 20:52:45 -0300

    Improved install script.

--- install-cassandra-trigger.sh
+++ install-cassandra-trigger.sh
@@ -1,58 +1,57 @@
 #!/bin/sh
 
-if [ ! $1 ]
-then
-	echo "First argument should be the Cassandra root directory"
+set -e
+
+jar_name="cassandra-logger*.jar"
+gradle_build_dir="build/libs"
+
+if [ ! $1 ]; then
+	echo "First argument should be a Cassandra 2.1+ root directory. You need to have write access to it."
 	exit 1
 fi
 
 cassandra_home=$1
-if [ ! -d $cassandra_home ]
-then
+if [ ! -d ${cassandra_home} ]; then
 	echo "Directory does not exist - $cassandra_home"
 	exit 1
 fi
 
-triggers_dir="$cassandra_home/conf/triggers"
-if [ ! -d $triggers_dir ]
-then 
+triggers_dir="${cassandra_home}/conf/triggers"
+if [ ! -d ${triggers_dir} ]; then
 	echo "Triggers directory does not exist ($triggers_dir)."
 	echo "Are you sure this is a valid Cassandra 2.1+ installation?"
 	exit 1
 fi
 
-if ! type "gradle" > /dev/null
-then
-	echo "Gradle command was not found. Make sure it is installed and available in the PATH."
+if ! type "gradle" > /dev/null; then
+	echo "Gradle command was not found. Make sure it was installed and added to the PATH."
 	exit 1
 fi
 
-echo "Compiling jar with Gradle..."
-if gradle clean assemble -q
-then
-	echo "Uninstalling old jar versions..."
-	rm -f "$triggers_dir/cassandra-logger*.jar"
-	
-	echo "Copying jar into Cassandra triggers directory..."
-	cp build/libs/cassandra-logger*.jar $triggers_dir
+echo "Building jar with Gradle..."
+gradle clean assemble -q
 
-	user=`whoami`
-	cassandra_pid=`pgrep -u $user -f cassandra`
-	if [ $cassandra_pid ]
-	then
-		echo "Cassandra is running with PID $cassandra_pid."
-		echo "Reloading triggers..."
-		if $cassandra_home/bin/nodetool -h localhost reloadtriggers
-		then
-			echo "Trigger installed and loaded successfuly. You can already use it on the CQL sheel."
-		else
-			echo "Something went wrong. Could not reload triggers."
-		fi
-	else
-		echo "Cassandra is not running. You can start it with $cassandra_home/bin/cassandra."
-		echo "The trigger was successfully installed nevertheless."
-	fi
+echo "Uninstalling old jar versions..."
+rm -f ${triggers_dir}/${jar_name}
+
+echo "Copying new jar into Cassandra triggers directory..."
+cp ${gradle_build_dir}/${jar_name}  ${triggers_dir}
+
+echo "The trigger was successfully installed."
+
+user=`whoami`
+cassandra_pid=`pgrep -u ${user} -f cassandra`
+
+if [ ${cassandra_pid} ]; then
+    echo "Cassandra is running for current user with PID ${cassandra_pid}. Atempting to reload triggers..."
+    if ${cassandra_home}/bin/nodetool -h localhost reloadtriggers; then
+        echo "Trigger loaded successfuly. You can already use it on the CQL sheel."
+    else
+        echo "Something went wrong. Could not reload triggers. Try restarting Cassandra manually."
+        exit 1
+    fi
 else
-    echo "Gradle build failed. Aborted."
-    exit 1
+    echo "Cassandra is not running. The trigger will be available next time it starts."
 fi
+
+exit 0
