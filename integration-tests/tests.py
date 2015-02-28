04bf1a3baaefb46ef4d0fcded8d472a6e83b136a
Felipe Dornelas <felipead@gmail.com>
2015-02-28 04:22:51 -0300

    Added pyhamcrest

--- integration-tests/tests.py
+++ integration-tests/tests.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 import uuid
 
 import pytest
+from hamcrest import *
 
 
 LOG_TRIGGER_NAME = "com.felipead.cassandra.logger.LogTrigger"
@@ -34,8 +36,20 @@ def insert_product_statement(session):
 # noinspection PyClassHasNoInit,PyMethodMayBeStatic,PyShadowingNames
 @pytest.mark.usefixtures("create_fixture_keyspace", "create_fixture_schema")
 class TestLogTrigger:
+
+    def select_log_entries(self, session, log_table_identifier):
+        return session.execute(
+            """
+            SELECT logged_keyspace, logged_table, logged_key, time, operation, updated_columns FROM %s
+            """
+            % log_table_identifier
+        )
+
     def test_create_log_entry_on_insert(self, session, fixture_keyspace,
                                         log_table_identifier, insert_product_statement):
+
+        now = datetime.now()
+
         shirt_id = uuid.uuid4()
         session.execute(insert_product_statement, [shirt_id, "shirt", 20])
         socks_id = uuid.uuid4()
@@ -44,22 +58,17 @@ class TestLogTrigger:
         session.execute(insert_product_statement, [jeans_id, "jeans", 10])
         product_ids = [shirt_id, socks_id, jeans_id]
 
-        rows = session.execute(
-            """
-            SELECT logged_keyspace, logged_table, logged_key, time, operation, updated_columns FROM %s
-            """
-            % log_table_identifier
-        )
+        log_entries = self.select_log_entries(session, log_table_identifier)
+        assert log_entries >= len(product_ids)
 
-        assert rows >= len(product_ids)
         matched_rows = 0
-        for (logged_keyspace, logged_table, logged_key, time, operation, updated_columns) in rows:
+        for (logged_keyspace, logged_table, logged_key, time, operation, updated_columns) in log_entries:
             if uuid.UUID(logged_key) in product_ids:
                 assert logged_keyspace == fixture_keyspace
                 assert logged_table == "product"
                 assert operation == "save"
-                assert updated_columns
-                print updated_columns, time
+                assert time > now
+                assert_that(updated_columns, has_items("name", "quantity"))
                 matched_rows += 1
 
         assert matched_rows == len(product_ids)
